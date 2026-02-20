c2974359e5fc9c4bec79094ed17a953fd25efbe1
Tim Shelton <tshelton@hawkdefense.com>
2026-02-20 00:12:38 -0600

    Use canonical pySigma Hawk backend in dev CLI

--- dev_cli.py
+++ dev_cli.py
@@ -1,8 +1,8 @@
 import argparse
 import json
-from sigma.parser import SigmaCollectionParser
-from sigma.configuration import SigmaConfiguration
-from pysigma_hawk_backend.hawk_backend import HawkBackend  
+from sigma.collection import SigmaCollection
+from sigma.pipelines.hawk import hawk_pipeline
+from sigma.backends.hawk.hawk import hawkBackend
 
 def main():
     parser = argparse.ArgumentParser(description="HAWK pySigma Backend CLI Tester")
@@ -13,13 +13,12 @@ def main():
     with open(args.rulefile, "r") as f:
         rule_text = f.read()
 
-    config = SigmaConfiguration()
-    rules = SigmaCollectionParser(rule_text, config).rules
-    backend = HawkBackend(config)
+    collection = SigmaCollection.from_yaml(rule_text)
+    backend = hawkBackend(processing_pipeline=hawk_pipeline())
+    converted = backend.convert(collection)
 
-    for rule in rules:
-        result = backend.convert(rule)
-        print(json.dumps(json.loads(result), indent=2))
+    for result in converted:
+        print(json.dumps(result, indent=2))
 
 if __name__ == "__main__":
     main()
